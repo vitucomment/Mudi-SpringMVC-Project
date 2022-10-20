2df8423cae8d17112b58528ef15dfcfeb87cab3d
vitucomment <victorpy.1999@gmail.com>
2022-10-20 13:37:16 -0300

    Connecting dataBase

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,30 +1,26 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.repository.PedidoRepository;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
 	@GetMapping("/home")
-	public String home(Model model) {
-		
-		Pedido pedido = new Pedido();
-		pedido.setNomeProduto("Smartphone Xiaomi Redmi Note 8");
-		pedido.setUrlImagem("https://m.media-amazon.com/images/I/51RIqTiRc8L._AC_SL1000_.jpg");
-		pedido.setUrlProduto("https://www.amazon.com.br/Smartphone-Xiaomi-Redmi-Note-Camera/dp/B07Z5BBG56");
-		pedido.setDescricao("Uma descrição qualquer para esse pedido");
-		
-		List<Pedido> pedidos = Arrays.asList(pedido);
-		
-		model.addAttribute("pedidos", pedidos);
-		
-		return "home";
+	public ModelAndView home() {
+		List<Pedido> pedidos = pedidoRepository.findAll();	
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("pedidos", pedidos);
+		return mv;
 	}
 }
