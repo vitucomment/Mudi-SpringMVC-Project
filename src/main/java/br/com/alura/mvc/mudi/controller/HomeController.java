ecd4c3f347c37f283c277a8d460bf44444769728
vitucomment <victorpy.1999@gmail.com>
2022-10-26 15:04:52 -0300

    adding cache

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,14 +1,12 @@
 package br.com.alura.mvc.mudi.controller;
 
-import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -25,7 +23,11 @@ public class HomeController {
 	
 	@GetMapping
 	public ModelAndView home() {
-		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE);	
+		
+		Sort sort = Sort.by("dataDaEntrega").descending();
+		PageRequest paginacao = PageRequest.of(0, 5, sort);
+		
+		List<Pedido> pedidos = pedidoRepository.findByStatus(StatusPedido.ENTREGUE, paginacao);	
 		ModelAndView mv = new ModelAndView("home");
 		mv.addObject("pedidos", pedidos);
 		return mv;
